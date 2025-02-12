a84f3152a194928d4c43d3992193e5b19e28abfa
Yves Kreimerman <yveskrei@gmail.com>
2025-02-12 16:30:27 +0200

    reworking codebase

--- research.py
+++ research.py
@@ -2,18 +2,18 @@ import json
 
 # Custom modules
 from utils.wrapper import Wrapper
+import utils.object_detection.evaluator as object_detection
 import utils.config as config
 
 model = Wrapper(
     model_path='./models/yolov9-m.onnx',
-    model_task=config.TASK_DETECTION,
     model_type=config.MODEL_YOLO
 )
 
 # Evaluate model
-results = model.evaluator.evaluate(
-    dataset_annotation='./datasets/val2017/annotation.json',
-    dataset_name='val2017',
+results = object_detection.evaluate_model(
+    model=model,
+    coco_annotation='./datasets/val2017/annotation.json'
 )
 
 # Save results to JSON
